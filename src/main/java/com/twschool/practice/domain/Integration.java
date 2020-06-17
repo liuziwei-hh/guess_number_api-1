a124d3246377e8ba21efde3d169f2f762ce496da
shalldi <shalldid@hotmail.com>
2020-06-17 20:45:45 +0800

    在Integration类中优化addPoint()方法，每执行三次，point会额外增加2分

--- src/main/java/com/twschool/practice/domain/Integration.java
+++ src/main/java/com/twschool/practice/domain/Integration.java
@@ -6,6 +6,12 @@ package com.twschool.practice.domain;
 public class Integration {
 
     private static final int ONCE_ADD_STEP = 3;
+    private static final int THREE_TIMES_WIN_BOUNS_COUNT = 3;
+    private static final int THREE_TIMES_WIN_BOUNS_POINT = 2;
+    private static final int FIVE_TIMES_WIN_BOUNS_COUNT = 5;
+    private static final int FIVE_TIMES_WIN_BOUNS_POINT = 3;
+
+    private int addCount = 0;
 
     private int point;
 
@@ -18,6 +24,12 @@ public class Integration {
     }
 
     public void addPoint() {
-        this.point += 3;
+        this.point += ONCE_ADD_STEP;
+        if (++addCount == THREE_TIMES_WIN_BOUNS_COUNT) {
+            this.point += THREE_TIMES_WIN_BOUNS_POINT;
+        }
+//        else if (addCount == FIVE_TIMES_WIN_BOUNS_COUNT) {
+//            this.point += FIVE_TIMES_WIN_BOUNS_POINT;
+//        }
     }
 }
