e09446eec4f41dbf41fa20cb5338014b253c279a
shalldi <shalldid@hotmail.com>
2020-06-17 21:16:10 +0800

    对用户增加积分系统，赢一次，获得3分

--- src/main/java/com/twschool/practice/domain/User.java
+++ src/main/java/com/twschool/practice/domain/User.java
@@ -1,13 +1,24 @@
 package com.twschool.practice.domain;
 
 public class User {
+
     private String id;
+    private Integration integration;
+
+    public User(String id) {
+        this.id = id;
+        integration = new Integration();
+    }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void win() {
+        this.integration.addPoint();
+    }
+
+    public int getPoint() {
+        return this.integration.getPoint();
     }
 }
