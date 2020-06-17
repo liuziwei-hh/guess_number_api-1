8c305ed69e36fca01821837d6c1537b11f371aa2
shalldi <shalldid@hotmail.com>
2020-06-17 20:34:00 +0800

    在Integration类中增加addPoint()方法，每执行一个point +3

--- src/main/java/com/twschool/practice/domain/Integration.java
+++ src/main/java/com/twschool/practice/domain/Integration.java
@@ -4,6 +4,9 @@ package com.twschool.practice.domain;
  * 积分类
  * */
 public class Integration {
+
+    private static final int ONCE_ADD_STEP = 3;
+
     private int point;
 
     public int getPoint() {
@@ -12,5 +15,9 @@ public class Integration {
 
     public void setPoint(int point) {
         this.point = point;
+    }
+
+    public void addPoint() {
+        this.point += 3;
     }
 }
