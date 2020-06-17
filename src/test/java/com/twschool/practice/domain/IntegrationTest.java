c0a55f0667f7648405e02a8bfbc9799af88f3ef7
shalldi <shalldid@hotmail.com>
2020-06-17 21:00:42 +0800

    新增测试方法，在初始化后执行subPoint返回-3

--- src/test/java/com/twschool/practice/domain/IntegrationTest.java
+++ src/test/java/com/twschool/practice/domain/IntegrationTest.java
@@ -17,57 +17,48 @@ public class IntegrationTest {
     @Test
     public void should_return_3_when_execute_method_addPoint() {
         Integration i = new Integration();
-        i.addPoint();
+        executeAddPointByCount(i, 1);
         Assert.assertEquals(3, i.getPoint());
     }
 
     @Test
     public void should_return_11_when_execute_3_times_method_addPoint() {
         Integration i = new Integration();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
+        executeAddPointByCount(i, 3);
         Assert.assertEquals(11, i.getPoint());
     }
 
     @Test
     public void should_return_20_when_execute_5_times_method_addPoint() {
         Integration i = new Integration();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
+        executeAddPointByCount(i, 5);
         Assert.assertEquals(20, i.getPoint());
     }
 
     @Test
     public void should_return_31_when_execute_8_times_method_addPoint() {
         Integration i = new Integration();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
+        executeAddPointByCount(i, 8);
         Assert.assertEquals(31, i.getPoint());
     }
 
     @Test
     public void should_return_40_when_execute_10_times_method_addPoint() {
         Integration i = new Integration();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
-        i.addPoint();
+        executeAddPointByCount(i, 10);
         Assert.assertEquals(40, i.getPoint());
+    }
+
+    @Test
+    public void should_return_burden_3_when_execute_subPoint() {
+        Integration i = new Integration();
+        i.subPoint();
+        Assert.assertEquals(-3, i.getPoint());
+    }
+
+    private void executeAddPointByCount(Integration integration, int count) {
+        for (int i = 0; i < count; i++) {
+            integration.addPoint();
+        }
     }
 }
