b39bef163a39966f4dbd742433d312dad4526c5f
shalldi <shalldid@hotmail.com>
2020-06-17 20:53:20 +0800

    根据新增5次重置积分计数次数完成功能

--- src/main/java/com/twschool/practice/domain/Integration.java
+++ src/main/java/com/twschool/practice/domain/Integration.java
@@ -5,13 +5,14 @@ package com.twschool.practice.domain;
  * */
 public class Integration {
 
+    private static final int INIT_WIN_BOUNS_COUNT = 0;
     private static final int ONCE_ADD_STEP = 3;
     private static final int THREE_TIMES_WIN_BOUNS_COUNT = 3;
     private static final int THREE_TIMES_WIN_BOUNS_POINT = 2;
     private static final int FIVE_TIMES_WIN_BOUNS_COUNT = 5;
     private static final int FIVE_TIMES_WIN_BOUNS_POINT = 3;
 
-    private int addCount = 0;
+    private int addCount = INIT_WIN_BOUNS_COUNT;
 
     private int point;
 
@@ -29,6 +30,7 @@ public class Integration {
             this.point += THREE_TIMES_WIN_BOUNS_POINT;
         } else if (addCount == FIVE_TIMES_WIN_BOUNS_COUNT) {
             this.point += FIVE_TIMES_WIN_BOUNS_POINT;
+            this.addCount = INIT_WIN_BOUNS_COUNT;
         }
     }
 }
