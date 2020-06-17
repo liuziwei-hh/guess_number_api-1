b66e2558d6d36e5d127df32bde1c9ed356305ea3
shalldi <shalldid@hotmail.com>
2020-06-17 20:21:01 +0800

    新增domain的Integration类，并且根据相应的测试类新增point属性及其Getter&Setter方法

---
+++ src/main/java/com/twschool/practice/domain/Integration.java
@@ -0,0 +1,16 @@
+package com.twschool.practice.domain;
+
+/**
+ * 积分类
+ * */
+public class Integration {
+    private int point;
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+}
