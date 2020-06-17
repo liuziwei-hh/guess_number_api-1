c675b9bbb8e8f5a656d6a99d9de552dcecdb1976
shalldi <shalldid@hotmail.com>
2020-06-17 21:23:10 +0800

    新增测试方法，测试用户连续赢的时候积分变化测试，且都同构

--- src/test/java/com/twschool/practice/domain/UserTest.java
+++ src/test/java/com/twschool/practice/domain/UserTest.java
@@ -17,4 +17,55 @@ public class UserTest {
         u.win();
         Assert.assertEquals(3, u.getPoint());
     }
+
+    @Test
+    public void should_return_11_when_user_win_3() {
+        User u = new User("test");
+        u.win();
+        u.win();
+        u.win();
+        Assert.assertEquals(11, u.getPoint());
+    }
+
+    @Test
+    public void should_return_20_when_user_win_5() {
+        User u = new User("test");
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        Assert.assertEquals(20, u.getPoint());
+    }
+
+    @Test
+    public void should_return_31_when_user_win_8() {
+        User u = new User("test");
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        Assert.assertEquals(31, u.getPoint());
+    }
+
+    @Test
+    public void should_return_40_when_user_win_10() {
+        User u = new User("test");
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        u.win();
+        Assert.assertEquals(40, u.getPoint());
+    }
+
 }
